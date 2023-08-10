cd518a409309527984ade114e80d63baaa181940
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-09 23:36:45 -0400

    Header'Component: add titile and buttons

---
+++ app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import {NoteIcon} from './Icons'
+
+export const Header = ({listname}) =>{
+
+	const create = () =>{
+		console.log('create')
+	}
+	const signout = () =>{
+		console.log('signout')
+	}
+	return(
+		<header className="list-header">
+			<h1><NoteIcon/> {listname}</h1>
+			<div className="button-container">
+				<button className="create" onClick={create}>ADD NEW</button>
+				<button className="signout" onClick={signout}>SIGN OUT</button>
+			</div>
+		</header>
+	)
+}
