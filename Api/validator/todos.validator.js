51f8cd1b5900a6553da61986e36f69c0b11fa636
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-09 23:41:44 -0400

    data are validated

---
+++ Api/validator/todos.validator.js
@@ -0,0 +1,21 @@
+import z from 'zod'
+
+const todoShema = z.object({
+	user_email: z.string({
+		invalid_type_error: 'todo email must be a string',
+		required_error: 'todo user_email is required'
+	}),
+	title: z.string({
+		invalid_type_error: 'todo title must be a string',
+		required_error: 'todo title is required'
+	}),
+	progress: z.number().int().min(0).max(10).default(5)
+})
+
+export const validate = (object) =>{
+	return todoShema.safeParse(object)
+}
+
+export const validatePartial = (object) =>{
+	return todoShema.partial().safeParse(object)
+}
