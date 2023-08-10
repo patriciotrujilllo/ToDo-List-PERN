2f014903b78d7e27d5a583a95795d19d40805ad8
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-09 23:46:45 -0400

    Create server, midleware todos

---
+++ Api/server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import { todosRouter } from './Controller/todo.controller.js'
+import {config} from 'dotenv'
+config()
+
+
+const app = express()
+app.use(express.json())
+
+app.use('/todos',todosRouter)
+
+const PORT = process.env.PORTAPI || '4000'
+
+app.listen(PORT, ()=>{
+	console.log(`server running on port ${PORT}`)
+})
+
