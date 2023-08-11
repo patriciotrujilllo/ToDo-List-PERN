b2722a11a43f57c38111d13f5360d928675770bd
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-10 23:30:53 -0400

    Create modal with css

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,7 +1,39 @@
 export const Modal = () =>{
+	
+	const mode = 'edit'
+
+	const handleChange = () =>{
+
+	}
+	const handleChangeRange = () =>{
+
+	}
 	return(
-		<div>
-            
+		<div className="overlay">
+			<div className="modal">
+				<div className="form-title">
+					<h3>Lets {mode} your task</h3>
+					<button>X</button>
+				</div>
+
+				<form>
+					<input 
+						required
+						maxLength={30}
+						placeholder="add your task"
+						name="title"
+						onChange={handleChange}
+					/>
+					<input 
+						required
+						type="range"
+						min="0"
+						max="100"
+						onChange={handleChangeRange}
+					/>
+					<button className={mode}>{mode}</button>
+				</form>
+			</div>
 		</div>
 	)
 }
