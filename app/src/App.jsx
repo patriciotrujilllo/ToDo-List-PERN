09fac0c02305eb0dd0b9a045b8158300b66db99e
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-10 23:34:48 -0400

    Create function with fetch to Api, it is save in a useState

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react'
 import { Header } from './components/Header'
+import { ListItem } from './components/ListItem'
 
 function App() {
+	const [tasks,setTasks] = useState([])
+
+	const Data = async() =>{
+		try{
+			const response = await fetch('http://localhost:4000/todos/pato3@gmail.com')
+			const json = await response.json()
+			setTasks(json)
+		}catch (err){
+			console.error(err)
+		}
+	}
+
+	useEffect(()=>{
+		Data()
+	},[])
+
 	return(
 		<main className='app'>
 			<Header listname={'To Do List'}/>
+			{
+				tasks?<ListItem tasks={tasks}/>:<span>...loading</span>
+			}
+			
 		</main>
 	)
 
