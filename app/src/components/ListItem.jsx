5ebfcdb485573e9e88e316bfde720569457ff1ff
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-10 23:32:00 -0400

    Create list of items

--- app/src/components/ListItem.jsx
+++ app/src/components/ListItem.jsx
@@ -1,7 +1,16 @@
-export const ListItem = () =>{
-	return(
-		<div>
-            
-		</div>
+import { Item } from './Item'
+
+export const ListItem = ({ tasks }) => {
+	//Orsenar por fecha
+	const sortedTasks = tasks.sort((a,b) => new Date(a.date) - new Date(b.date))
+	return (
+		<ul className='list-item'>
+			{
+				sortedTasks?
+					sortedTasks.map((sortedTasks) => (
+						<Item key={sortedTasks.id} task={sortedTasks}/>
+					)):<span>...loading</span>
+			}
+		</ul>
 	)
 }
