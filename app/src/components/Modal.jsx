b84a47b7388bca82bfe9d64a9137cc96f1ac8cf7
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-12 22:36:53 -0400

    create handleChange and handleSubmit

--- app/src/components/Modal.jsx
+++ app/src/components/Modal.jsx
@@ -1,34 +1,72 @@
-export const Modal = () =>{
-	
-	const mode = 'edit'
+import { useState } from 'react'
+import { createTodo,editTodo } from '../services/todo.services.js'
 
-	const handleChange = () =>{
+export const Modal = ({mode,setShowModal,setTasks,tasks,task}) =>{
 
+	const [data,setData] = useState({
+		user_email: mode==='edit' ? task.user_email: 'pato3@gmail.com',
+		title: mode==='edit' ? task.title: '',
+		progress: mode==='edit' ? task.progress: 0
+	})
+
+	const handleChangeTitle = (e) =>{
+		const title = e.target.value
+		setData(prevData => ({
+			...prevData,
+			title: title
+		}))
 	}
-	const handleChangeRange = () =>{
-
+	const handleChangeRange = (e) =>{
+		const progress = e.target.value
+		setData(prevData => ({
+			...prevData,
+			progress: parseInt(progress)
+		}))
+	}
+	const handleSubmitCreate = async(e,tasks) =>{
+		e.preventDefault()
+		const newtodo = await createTodo(data)
+		
+		setTasks([...tasks, newtodo])
+		setShowModal(false)
+	}
+	const handleSubmitEdit = async(e,task) =>{
+		e.preventDefault()
+		const updateTask =  await editTodo(data,task.id)
+		const idtask = tasks.findIndex(t=>t.id===task.id)
+		const newTaskDom = structuredClone(tasks)
+		newTaskDom[idtask]={
+			...newTaskDom[idtask],
+			...updateTask
+		}
+		setTasks(newTaskDom)
+		setShowModal(false)
 	}
 	return(
 		<div className="overlay">
 			<div className="modal">
 				<div className="form-title">
 					<h3>Lets {mode} your task</h3>
-					<button>X</button>
+					<button onClick={()=>setShowModal(false)}>X</button>
 				</div>
 
-				<form>
+				<form onSubmit={mode==='edit'?(e)=>handleSubmitEdit(e,task):(e)=>handleSubmitCreate(e,tasks)}>
 					<input 
 						required
 						maxLength={30}
 						placeholder="add your task"
 						name="title"
-						onChange={handleChange}
+						value={data.title}
+						onChange={handleChangeTitle}
 					/>
+					<label id='range' >Select yor current progress</label>
 					<input 
 						required
 						type="range"
-						min="0"
-						max="100"
+						id='range'
+						min={0}
+						max={100}
+						value={data.progress}
 						onChange={handleChangeRange}
 					/>
 					<button className={mode}>{mode}</button>
