7990a44651f1a44743f9fc63869773ceeeb6cc50
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-12 22:32:24 -0400

    add props

--- app/src/components/ListItem.jsx
+++ app/src/components/ListItem.jsx
@@ -1,14 +1,15 @@
 import { Item } from './Item'
 
-export const ListItem = ({ tasks }) => {
+export const ListItem = ({ setTasks,tasks }) => {
 	//Orsenar por fecha
 	const sortedTasks = tasks.sort((a,b) => new Date(a.date) - new Date(b.date))
+
 	return (
 		<ul className='list-item'>
 			{
 				sortedTasks?
 					sortedTasks.map((sortedTasks) => (
-						<Item key={sortedTasks.id} task={sortedTasks}/>
+						<Item key={sortedTasks.id} task={sortedTasks} setTasks={setTasks} tasks={tasks}/>
 					)):<span>...loading</span>
 			}
 		</ul>
