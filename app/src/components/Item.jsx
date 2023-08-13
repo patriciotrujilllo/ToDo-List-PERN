b7cfef6d41064b46c3efcee33da2007f2e10d03c
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-12 22:36:04 -0400

    add state showModal and add props

--- app/src/components/Item.jsx
+++ app/src/components/Item.jsx
@@ -1,7 +1,10 @@
 import { CircleCheck } from './Icons'
 import { ProgressBar } from './ProgressBar'
+import { Modal } from './Modal'
+import { useState } from 'react'
 
-export const Item = ({task}) =>{
+export const Item = ({task,setTasks,tasks}) =>{
+	const [showModal,setShowModal] = useState(false)
 	return(
 		<li className="item">
 			<div className='info-container'>
@@ -11,10 +14,13 @@ export const Item = ({task}) =>{
 			</div>
 
 			<div className='button-container'>
-				<button className='edit'>EDIT</button>
+				<button className='edit' onClick={()=>setShowModal(true)}>EDIT</button>
 				<button className='delete'>DELETE</button>
 
 			</div>
+			{
+				showModal && <Modal mode={'edit'} setShowModal={setShowModal} task={task} setTasks={setTasks} tasks={tasks}/>
+			}
 		</li>
 	)
 }
