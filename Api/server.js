b0861973dae8d4e2a7c6c98c1b011d338738e2a0
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-16 12:59:05 -0400

    add route login

--- Api/server.js
+++ Api/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import { todosRouter } from './middleware/todo.controller.js'
 import { usersRouter } from './middleware/user.router.js'
+import { loginRouter } from './middleware/login.js'
 import {config} from 'dotenv'
 config()
 
@@ -10,6 +11,7 @@ app.use(express.json())
 
 app.use('/todos',todosRouter)
 app.use('/users',usersRouter)
+app.use('/login',loginRouter)
 
 const PORT = process.env.PORTAPI || '4000'
 
