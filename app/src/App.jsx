8e0e397d2dc7beadb1af6e8f9bf44bb4d3de8777
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-16 12:57:58 -0400

    add useState loading

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -7,12 +7,14 @@ import { useCookies } from 'react-cookie'
 function App() {
 	const [tasks,setTasks] = useState([])
 	const [cookies] = useCookies(null)
+	const [loading,setLoading] = useState(false)
 	const authToken = cookies && cookies.user && cookies.user.token ? cookies.user.token : null
 
 	const email = cookies && cookies.user && cookies.user.email ? cookies.user.email : null
 
 
 	const Data = async() =>{
+		setLoading(true)
 		try{
 			// eslint-disable-next-line no-undef
 			const response = await fetch(`${process.env.REACT_APP_URL}/todos/${email}`)
@@ -20,14 +22,18 @@ function App() {
 			setTasks(json)
 		}catch (err){
 			console.error(err)
+		}finally{
+			setLoading(false)
 		}
 	}
 
 	useEffect(()=>{
+		
 		if(authToken){
 			Data()
 		} 
-	},[])
+		
+	},[],loading)
 
 	return(
 		<main className='app'>
@@ -35,7 +41,7 @@ function App() {
 				<>
 					<Header listname={'To Do List'} setTasks={setTasks} tasks={tasks}/>
 			
-					{tasks?<ListItem setTasks={setTasks} tasks={tasks}/>:<span>...loading</span>}
+					{loading?<span>...loading</span>:<ListItem setTasks={setTasks} tasks={tasks}/>}
 				</>
 				:<Auth />
 			}
