236bdd29a528c297bd90502de1660a26bb444b70
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-17 18:50:44 -0400

    change [ ] for null

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -6,7 +6,7 @@ import { useCookies } from 'react-cookie'
 import { getTodo,setToken } from './services/todo.services'
 
 function App() {
-	const [tasks,setTasks] = useState([])
+	const [tasks,setTasks] = useState(null)
 	const [cookies] = useCookies(null)
 	const [loading,setLoading] = useState(false)
 	const authToken = cookies && cookies.user && cookies.user.token ? cookies.user.token : null
@@ -16,7 +16,6 @@ function App() {
 	const Data = async() =>{
 		setLoading(true)
 		try{
-			// eslint-disable-next-line no-undef
 			setToken(authToken)
 			const response = await getTodo(email)
 			setTasks(response)
@@ -33,11 +32,10 @@ function App() {
 			Data()
 		} 
 		
-	},[],loading)
-
+	},[authToken])
 	return(
 		<main className='app'>
-			{authToken?
+			{tasks && authToken?
 				<>
 					<Header listname={'To Do List'} setTasks={setTasks} tasks={tasks}/>
 			
