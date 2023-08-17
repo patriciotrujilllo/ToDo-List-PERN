d7bf361a28f4566bc3da3e3612f0025542c9a5a7
patriciotrujilllo <patriciotrujilloarias@gmail.com>
2023-08-17 18:49:13 -0400

    update

--- app/src/components/ListItem.jsx
+++ app/src/components/ListItem.jsx
@@ -2,14 +2,13 @@ import { Item } from './Item'
 
 export const ListItem = ({ setTasks,tasks }) => {
 	//Orsenar por fecha
-	const sortedTasks = tasks.sort((a,b) => new Date(a.date) - new Date(b.date))
-
+	// const task = tasks.sort((a,b) => new Date(a.date) - new Date(b.date))
 	return (
 		<ul className='list-item'>
 			{
-				sortedTasks?
-					sortedTasks.map((sortedTasks) => (
-						<Item key={sortedTasks.id} task={sortedTasks} setTasks={setTasks} tasks={tasks}/>
+				tasks?
+					tasks.map((task) => (
+						<Item key={task.id} task={task} setTasks={setTasks} tasks={tasks}/>
 					)):<span>...loading</span>
 			}
 		</ul>
